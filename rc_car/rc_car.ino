423976634ca1a02170a5553c0d28c9900f202980
subodhstha <shresthasubodh73@gmail.com>
2023-04-02 22:01:52 +0545

    rc car

--- rc_car/rc_car.ino
+++ rc_car/rc_car.ino
@@ -1,28 +1,31 @@
 #include<AFMotor.h>
-char cm;
-char s;
+#include<SoftwareSerial.h>  //for convering normal ports to serial input
+
+// char cm;
+// char s;
 AF_DCMotor m1(1), m2(2), m3(3), m4(4);
-char cm;
+SoftwareSerial myserial(A0, A1);
+
 void setup()
 {
-  Serial.begin(9600);
+  myserial.begin(9600);
 }
 
 void front(int speed)
 {
   setSpeed(speed);
-  m1.run(FORWARD);
+  m1.run(BACKWARD);
   m2.run(FORWARD);
   m3.run(BACKWARD);
-  m4.run(FORWARD);
+  m4.run(BACKWARD);
 }
 
 void left(int speed)
 {
   setSpeed(speed);
-  m2.run(FORWARD);
   m1.run(FORWARD);
-  m3.run(FORWARD);
+  m2.run(BACKWARD);
+  m3.run(BACKWARD);
   m4.run(BACKWARD);
 }
 
@@ -30,26 +33,26 @@ void right(int speed)
 {
   setSpeed(speed);
   m1.run(BACKWARD);
-  m2.run(BACKWARD);
-  m3.run(BACKWARD);
+  m2.run(FORWARD);
+  m3.run(FORWARD);
   m4.run(FORWARD);
 }
 
 void frontRight(int speed)
 {
   setSpeed(speed);
-  m1.run(FORWARD);
+  m1.run(BACKWARD);
   m2.run(FORWARD);
   m3.run(FORWARD);
-  m4.run(BACKWARD);
+  m4.run(FORWARD);
 }
 
 void frontLeft(int speed)
 {
   setSpeed(speed);
-  m1.run(BACKWARD);
+  m1.run(FORWARD);
   m2.run(BACKWARD);
-  m3.run(FORWARD);
+  m3.run(BACKWARD);
   m4.run(BACKWARD);
 }
 
@@ -70,17 +73,20 @@ void backLeft(int speed)
   m3.run(BACKWARD);
   m4.run(FORWARD);
 }
+
 void back(int speed)
 {
   setSpeed(speed);
-  m1.run(BACKWARD);
+  m1.run(FORWARD);
   m2.run(BACKWARD);
   m3.run(FORWARD);
-  m4.run(BACKWARD);
+  m4.run(FORWARD);
 }
 
 void stop()
 {
+  // setSpeed(speed);
+  
   m1.run(RELEASE);
   m2.run(RELEASE);
   m3.run(RELEASE);
@@ -95,20 +101,96 @@ void setSpeed(int speed)
   m4.setSpeed(speed);
 }
 
-
-void loop()
+void loop() 
 {
-  if (Serial.available())
+  if(myserial.available())
   {
-    cm = Serial.read()
-    println()
-    switch(cm)
+    char data = myserial.read();
+    switch(data)
     {
-      case cm == 'S'
-      stop();
-      break;
-      
+      case '1':
+        sp = 255 * 0.1;
+        break;
 
+      case '2':
+        sp = 255 * 0.2;
+        break;
+
+      case '3':
+        sp = 255 * 0.3;
+        break;
+
+      case '4':
+        sp = 255 * 0.4;
+        break;
+
+      case '5':
+        sp = 255 * 0.5;
+        break;
+
+      case '6':
+        sp = 255 * 0.6;
+        break;
+
+      case '7':
+        sp = 255 * 0.7;
+        break;
+
+      case '8':
+        sp = 255 * 0.8;
+        break;
+
+      case '9':
+        sp = 255 * 0.9;
+        break;
+
+      case 'q':
+        sp = 255;
+        break;
+
+      case 'F':
+        front(sp);
+        break;
+      
+      case 'B':
+        back(sp);
+        break;
+
+      case 'L':
+        left(255);
+        break;
+
+      case 'R':       
+        right(255);
+        break;
+
+      case 'G':
+        sp = 255;
+        frontLeft(sp);
+        break;
+
+      case 'I':
+        frontRight(sp);
+        break;
+
+      case 'H':
+        backLeft(sp);
+        break;
+
+      case 'J':
+        backRight(sp);
+        break;
+
+      case 'S':
+        stop();
+        break;
+      
+      case 'D':
+        stop();
+        break;
+
+      default:
+        stop();
     }
   }
 }
